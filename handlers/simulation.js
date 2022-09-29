dbbb6e341ae33bfb77ac5fe60cbc5958db5a3336
credifythanhtran <110369112+credifythanhtran@users.noreply.github.com>
2022-09-29 14:58:19 +0700

    Dev (#29)
    * hard code product input (#28)

--- handlers/simulation.js
+++ handlers/simulation.js
@@ -1,12 +1,26 @@
 const simulation = async (req, res, { credify }) => {
   // call credify core to get simulation configuration
   try {
-      const productType = req.body.productType
-      const providerIds = req.body.providerIds || ["09de7359-7f29-41a0-bd07-095d1ce7f85d"]
-      const inputs = req.body.inputs
-      const response = await credify.offer.simulate(productType, providerIds, inputs)
+    const productType = req.body.productType;
+    const providerIds = req.body.providerIds || [
+      "09de7359-7f29-41a0-bd07-095d1ce7f85d",
+    ];
+    let inputs = req.body.inputs;
+    inputs = {
+      ...inputs,
+      product: {
+        manufacturer: "test",
+        category: "ahihi",
+        name: "hehe",
+      },
+    };
+    const response = await credify.offer.simulate(
+      productType,
+      providerIds,
+      inputs
+    );
 
-      return res.status(200).json({ success: true, data: response.list })
+    return res.status(200).json({ success: true, data: response.list });
   } catch (error) {
       return res.status(500).json({ message: error.message })
   }
